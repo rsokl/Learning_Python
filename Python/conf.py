100681b18ecfb4b824ca24c67bda8742384564e0
Ryan Soklaski <ry26099@mit.edu>
2019-02-16 16:10:32 -0500

    (sphinx 1.7.9) restore pygments syntax highlighting

--- Python/conf.py
+++ Python/conf.py
@@ -76,7 +76,7 @@ language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
